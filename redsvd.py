444485109b722bc480799dc776511469e24f102e
taneishi <taneishi@users.noreply.github.com>
2020-01-25 17:57:58 +0900

    cancor moved pca project.

--- redsvd.py
+++ redsvd.py
@@ -1,16 +1,19 @@
-import os
-import gzip
+# %%
 import numpy as np
-from sklearn import datasets
 from scipy import linalg, sparse
+from sklearn import datasets
 import timeit
+import gzip
 
+# %%
 SVD_EPS = 1e-4
 
+# %%
 def svd(mat, k=10):
     from scipy.sparse.linalg import svds
     return svds(mat,k=k)
 
+# %%
 def GramSchmidt(mat):
     for i in range(mat.shape[1]):
         for j in range(i):
@@ -23,6 +26,7 @@ def GramSchmidt(mat):
             return
         mat[:,i] *= 1.0 / norm
 
+# %%
 def redsvd(A, k=10):
     O = np.random.randn(A.shape[0]*k).reshape(A.shape[0],k)
     Y = A.T.dot(O)
@@ -34,10 +38,11 @@ def redsvd(A, k=10):
     GramSchmidt(Z)
     C = np.dot(Z.T, B)
 
-    U,S,V = linalg.svd(C)
+    U, S, V = linalg.svd(C)
 
     return S
 
+# %%
 if __name__ == '__main__':
     filename = 'news20.binary.gz'
     A, target = datasets.load_svmlight_file(gzip.open(filename))
@@ -45,3 +50,5 @@ if __name__ == '__main__':
     start_time = timeit.default_timer()
     print(redsvd(A))
     print(timeit.default_timer() - start_time)
+
+# %%
